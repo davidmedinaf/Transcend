b8f7bcd33b50a2b41f80a7e3c1dfc3ee8997964c
DavidM <david.medina@navigaglobal.com>
2026-06-07 12:55:14 +0200

    Cache-bust PWA icons with versioned filenames

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icons/icon-192x192-v2.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icons/icon-512x512-v2.png",
         sizes: "512x512",
         type: "image/png",
       },
